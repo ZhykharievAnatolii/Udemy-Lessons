e02a76b86e8b0af9950cd709365a05487ecac084
ZhykharievAnatolii <nekijt87@gmail.com>
2023-02-02 18:59:28 +0200

    upd

---
+++ lesson1/index.js
@@ -0,0 +1,15 @@
+// console.log(1)
+
+let name='name';
+const name2='name2';
+var name3='name3';
+let number=5;
+number=10;
+
+const obj={
+    a:10,
+}
+obj.a=11;
+console.log(obj)
+
+var name4='Anatolii';
