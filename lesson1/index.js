f1e1f733e1a6b5672866aff092a330fc44dcc8f9
ZhykharievAnatolii <nekijt87@gmail.com>
2023-02-03 21:08:12 +0200

    fin upd

--- lesson1/index.js
+++ lesson1/index.js
@@ -27,13 +27,41 @@
 // };
 // console.log(result);
 
-const obj={
-    name:'Anatolii',
-    age:35,
-    isMarried:true,
-};
-console.log(obj.age)
-console.log(obj['name']);
+// const obj={
+//     name:'Anatolii',
+//     age:35,
+//     isMarried:true,
+// };
+// console.log(obj.age)
+// console.log(obj['name']);
+//
+// let arr=['plum.imj','orange.png','apple.jpeg'];
+// console.log(arr[1])
 
-let arr=['plum.imj','orange.png','apple.jpeg'];
-console.log(arr[1])
+
+// const arr=[1,2,3];
+// console.log(arr[1])
+// const obj={a:1,b:2,c:3};
+// const obj2={
+//     Anna:500,
+//     Alice:800,
+// }
+
+const arr=['a','b','c'];
+
+arr[10]=356;
+// console.log(arr[10])
+console.log(arr)
+const obj={
+    'a':0,
+    'b':1,
+    'c':2
+};
+
+const b='b';
+
+obj.c=12345;
+obj['d']=123456;
+// console.log(arr[1]);
+console.log(obj.d);
+console.log(obj['b'])
