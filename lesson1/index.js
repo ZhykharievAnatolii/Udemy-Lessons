b32939c451fbe8c7240944c0aa7e0cbaf87e2091
ZhykharievAnatolii <nekijt87@gmail.com>
2023-02-03 20:30:33 +0200

    upd

--- lesson1/index.js
+++ lesson1/index.js
@@ -1,28 +1,39 @@
 // 'use strict'
 
-a=15;
-console.log(a);
-// console.log(1)
-// console.log(name);
-let name='name';
-const name2='name2';
-var name3='name3';
-let number=5;
-number=10;
+// a=15;
+// console.log(a);
+// // console.log(1)
+// // console.log(name);
+// let name='name';
+// const name2='name2';
+// var name3='name3';
+// let number=5;
+// number=10;
 
-const obj={
-    a:10,
-}
-obj.a=11;
+// const obj={
+//     a:10,
+// }
+// obj.a=11;
 // console.log(obj)
 // console.log(name4)
-var name4='Anatolii';
+// var name4='Anatolii';
+//
+// {
+//     let result =50;
+// };
+//     console.log(result);
+// {
+//     var result =50;
+// };
+// console.log(result);
 
-{
-    let result =50;
+const obj={
+    name:'Anatolii',
+    age:35,
+    isMarried:true,
 };
-    console.log(result);
-{
-    var result =50;
-};
-console.log(result);
+console.log(obj.age)
+console.log(obj['name']);
+
+let arr=['plum.imj','orange.png','apple.jpeg'];
+console.log(arr[1])
