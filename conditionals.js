349f3463c523b5982e53718429bea93e7b816e3d
ZhykharievAnatolii <nekijt87@gmail.com>
2023-02-16 19:48:44 +0200

    add tern

--- conditionals.js
+++ conditionals.js
@@ -5,19 +5,28 @@
 //     run some other code instead
 // };
 // условие внутри круглых скобок должно быть преобразовано к булиновому типу true, false
-if(4==9){
-    console.log('Ok!')
+// if(4==9){
+//     console.log('Ok!')
+// }else {
+//     console.log('Error!')
+// };
+//
+// if(1){
+//     console.log('Ok!')
+// }else {
+//     console.log('Error!')
+// };
+// if(true){
+//     console.log('Ok!')
+// }else {
+//     console.log('Error!')
+// };
+const num=50;
+if(num<49){
+    console.log('false!')
+}else if(num>100){
+    console.log('false!')
 }else {
-    console.log('Error!')
+    console.log('OK!')
 };
-
-if(1){
-    console.log('Ok!')
-}else {
-    console.log('Error!')
-};
-if(true){
-    console.log('Ok!')
-}else {
-    console.log('Error!')
-}
+(num==47)?console.log('OK!'):console.log('false!');
