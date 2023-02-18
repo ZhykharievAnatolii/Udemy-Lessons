3271dfc14746a7b98252e96d5f74dcde43ad4d9a
ZhykharievAnatolii <nekijt87@gmail.com>
2023-02-18 18:15:52 +0200

    upd operators

--- logicalOperators.js
+++ logicalOperators.js
@@ -33,13 +33,68 @@
 // Undefined
 //
 // Все остальное всегда true
-const hamburger=3;
-const fries=1;
-const cola=0;
-if(hamburger===3&&cola===1&&fries===1){
-    console.log('Cool!!!' )
-}else {
-    console.log('No!')
-};
+// const hamburger=3;
+// const fries=1;
+// const cola=1;
+// console.log((hamburger===3&&cola&&fries===1))
+//
+// if(hamburger===3&&cola===1&&fries===1){
+//     console.log('Cool!!!' )
+// }else {
+//     console.log('No!')
+// };
+
+// Оператор И возвращает первое ложное значение на котором он остановился
+// Если все правдивы, то вернет последний
+//
+// console.log(0&&5);
+// console.log(true&&5);
+// console.log(null&&5);
+// console.log(0&&'hfefje');
+// console.log(1&&5);
+
+// console.log( !1 && 2 || !3 );
+
+// const hamburger=1;
+// const fries=0;
+// const cola=2;
+//
+// if(hamburger || cola || fries){
+//     console.log('Cool!!!' )
+// }else {
+//     console.log('No!')
+// };
+
+// Как только || находит true, то он перестает работать
+// Если все false, то возвращает последнее ложное значение
+// const hamburger=1;
+// const fries=3;
+// const cola=2;
+// const nuggets=3;
+//
+// if(hamburger===3 && cola ===2 || fries===3&&nuggets){
+//     console.log('Cool!!!' )
+// }else {
+//     console.log('No!')
+// };
+
+// console.log(!false);
+// console.log(!0);
+// console.log(!1)
+
+// console.log( NaN || 2 || undefined );
+// console.log( NaN && 2 && undefined );
+// console.log( 1 && 2 && 3 );
+// console.log( !1 && 2 || !3 );
 
 
+console.log( 25 || null && !3 );
+
+
+console.log( NaN || null || !3 || undefined || 5);
+
+
+console.log( NaN || null && !3 && undefined || 5);
+
+
+console.log( 5 === 5 && 3 > 1 || 5);
