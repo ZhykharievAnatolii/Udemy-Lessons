dbb3340114707c055f6825cf8e693936314b58d4
ZhykharievAnatolii <nekijt87@gmail.com>
2023-02-19 21:04:55 +0200

    upd cycl in cycl

--- cycles.js
+++ cycles.js
@@ -109,3 +109,12 @@ let num=50;
 //     // Не трогаем
 //     return arrayOfNumbers;
 // }
+
+for(let i=0;i<3;i++){
+    console.log(i);
+    for(let j=0;j<3;j++){
+        console.log(j)
+    }
+}
+
+// внутри вложенного цикла пишем не i а j
