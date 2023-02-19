89ad2424910ea9b266256110ce8ad5803b4f3388
ZhykharievAnatolii <nekijt87@gmail.com>
2023-02-19 18:17:52 +0200

    add tasks

--- cycles.js
+++ cycles.js
@@ -19,10 +19,63 @@ let num=50;
 //     num++
 // };
 
-for(let i=1;i<10;i++){
-   if(i===6){
-       // break
-       continue
-   }
-    console.log(i);
+// for(let i=1;i<10;i++){
+//    if(i===6){
+//        // break
+//        continue
+//    }
+//     console.log(i);
+// }
+// При помощи цикла выведите числа от 5 до 10 в консоль. 5 и 10 включительно. Цикл можно использовать любой
+// Место для первой задачи
+function firstTask() {
+    for(let i=5;i<=10;i++){
+        console.log(i)
+    }
+
+
+}
+
+// Место для второй задачи
+function secondTask() {
+    // Пишем решение вот тут
+
+
+}
+
+// Место для третьей задачи
+function thirdTask() {
+    // Пишем решение вот тут
+
+
+}
+
+// Место для четвертой задачи
+
+// Цикл, который нужно переписать:
+
+// for (let i = 2; i <= 16; i++) {
+//     if (i % 2 === 0) {
+//         continue;
+//     } else {
+//         console.log(i);
+//     }
+// }
+
+function fourthTask() {
+    // Пишем решение вот тут
+
+
+}
+
+// Место для пятой задачи
+
+function fifthTask() {
+    const arrayOfNumbers = [];
+
+    // Пишем решение вот тут
+
+
+    // Не трогаем
+    return arrayOfNumbers;
 }
