bd15edaf156a5a021b4b0f1c99ea20e7142a671a
ZhykharievAnatolii <nekijt87@gmail.com>
2023-02-20 20:50:01 +0200

    upd cycles

--- cyclesTasks.js
+++ cyclesTasks.js
@@ -84,3 +84,55 @@
 //     result +='\n';
 //
 // }
+/* Задание на урок:
+
+1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
+
+2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
+отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит -
+возвращаем пользователя к вопросам опять
+
+3) При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
+"Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше -
+"Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
+
+4) Потренироваться и переписать цикл еще двумя способами*/
+
+// let numberOfFilms= +prompt('Сколько фильмов вы уже посмотрели?');
+
+const personalMovieDB={
+    count:numberOfFilms,
+    movies:{},
+    actors:{},
+    genres:[],
+    privat:false
+
+};
+
+for(let i=0;i<2;i++){
+    const firstQuestion=prompt('Один из последних просмотренных фильмов?');
+    const secondQuestion=+prompt('На сколько оцените его?');
+    if(firstQuestion!=''&&!=firstQuestion.length>=50&&firstQuestion!=null){
+        personalMovieDB.movies[firstQuestion]=secondQuestion;
+        console.log("DONE!")
+    }else{
+        console.log(err);
+        i--;
+    }
+
+}
+if(personalMovieDB.count<10){
+    console.log("Просмотрено довольно мало фильмов")
+} else if(personalMovieDB.count>=10&&personalMovieDB<30){
+    console.log(""Вы классический зритель")
+} else if(personalMovieDB.count>=30){
+    console.log("Вы киноман")
+} else console.log('err!')
+// const thirdQuestion=prompt('Один из последних просмотренных фильмов?');
+// const fourthQuestion= +prompt('На сколько оцените его?');
+
+const movies ={
+};
+// personalMovieDB.movies[firstQuestion]=secondQuestion;
+// personalMovieDB.movies[thirdQuestion]=fourthQuestion;
+// console.log(personalMovieDB);
