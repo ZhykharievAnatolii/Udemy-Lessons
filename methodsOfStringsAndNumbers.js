c2a89bc471f0400069f63d101a3bab67d1ec6b75
ZhykharievAnatolii <nekijt87@gmail.com>
2023-02-24 20:28:34 +0200

    upd aboth methods of strings

---
+++ methodsOfStringsAndNumbers.js
@@ -0,0 +1,16 @@
+"use strict"
+
+const str='teSt';
+const arr=[1,2,3,4,5,6,7,8]
+// console.log(arr.length)
+
+// console.dir(Number)
+// console.log(str[2])
+// console.log(str.toUpperCase())
+// console.log(str)
+const fruit='Some fruit';
+console.log(fruit.indexOf('fruit'));
+const logg='Hello world!';
+console.log(logg.slice(6));
+console.log(logg.substr(6,11));
+console.log(logg.substring(6));
