2030daaaf6bfec14bfff3aceadc69c0456ff0be3
ZhykharievAnatolii <nekijt87@gmail.com>
2023-02-28 19:20:23 +0200

    upd task for functions

--- methodsOfStringsAndNumbers.js
+++ methodsOfStringsAndNumbers.js
@@ -8,9 +8,19 @@ const arr=[1,2,3,4,5,6,7,8]
 // console.log(str[2])
 // console.log(str.toUpperCase())
 // console.log(str)
-const fruit='Some fruit';
-console.log(fruit.indexOf('fruit'));
-const logg='Hello world!';
-console.log(logg.slice(6));
-console.log(logg.substr(6,11));
-console.log(logg.substring(6));
+// const fruit='Some fruit';
+// console.log(fruit.indexOf('fruit'));
+// const logg='Hello world!';
+// console.log(logg.slice(6));
+// console.log(logg.substr(6,11));
+// console.log(logg.substring(6));
+
+// Math- встроенная библиотека в обьект window
+
+const num= 12.2
+console.log(Math.round(num))
+const test='12.2px';
+console.log(parseInt(test))
+console.log(parseFloat(test))
+// parseInt()- переводит число в другой метод исчисления
+// parseFloat()- возвращает число с плавающей точкой
