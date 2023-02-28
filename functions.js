30359db68e413b40f097e385a173c3bbf322d51f
ZhykharievAnatolii <nekijt87@gmail.com>
2023-02-28 19:33:29 +0200

    add index.html

--- functions.js
+++ functions.js
@@ -176,5 +176,21 @@ genres
 P.S. Функции вызывать не обязательно*/
 
 'use strict';
+let numberOfFilms;
+function start() {
+    numberOfFilms=+prompt('Сколько фильмов Вы уже посмотрели?');
+    while (numberOfFilms===''|| numberOfFilms===null||isNaN(numberOfFilms)){
+        numberOfFilms=+prompt('Сколько фильмов Вы уже посмотрели?')
+    }
+}
+start();
+const personalMovieDB={
+    count:numberOfFilms,
+    movies:{},
+    actors:{},
+    genres:[],
+    privat:false
 
+
+}
 // Код возьмите из предыдущего домашнего задания
