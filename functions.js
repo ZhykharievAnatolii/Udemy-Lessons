2570c0536a8adc8e1dab8aae8a001f3efa06381b
ZhykharievAnatolii <nekijt87@gmail.com>
2023-02-28 19:44:18 +0200

    upd tasks

--- functions.js
+++ functions.js
@@ -162,18 +162,7 @@
 //
 //     return str;
 // }
-/* Задание на урок:
 
-1) Первую часть задания повторить по уроку
-
-2) Создать функцию showMyDB, которая будет проверять свойство privat. Если стоит в позиции
-false - выводит в консоль главный объект программы
-
-3) Создать функцию writeYourGenres в которой пользователь будет 3 раза отвечать на вопрос
-"Ваш любимый жанр под номером ${номер по порядку}". Каждый ответ записывается в массив данных
-genres
-
-P.S. Функции вызывать не обязательно*/
 
 'use strict';
 let numberOfFilms;
@@ -193,4 +182,56 @@ const personalMovieDB={
 
 
 }
-// Код возьмите из предыдущего домашнего задания
+
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++) {
+        const a = prompt('Один из последних просмотренных фильмов?', ''),
+            b = prompt('На сколько оцените его?', '');
+
+        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+            personalMovieDB.movies[a] = b;
+            console.log('done');
+        } else {
+            console.log('error');
+            i--;
+        }
+    }
+}
+rememberMyFilms();
+function detectPersonalLevel() {
+    if (personalMovieDB.count < 10) {
+        console.log("Просмотрено довольно мало фильмов");
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+        console.log("Вы классический зритель");
+    } else if (personalMovieDB.count >= 30) {
+        console.log("Вы киноман");
+    } else {
+        console.log("Произошла ошибка");
+    }
+
+};
+detectPersonalLevel();
+
+console.log(personalMovieDB);
+/* Задание на урок:
+
+1) Первую часть задания повторить по уроку
+
+2) Создать функцию showMyDB, которая будет проверять свойство privat. Если стоит в позиции
+false - выводит в консоль главный объект программы
+
+3) Создать функцию writeYourGenres в которой пользователь будет 3 раза отвечать на вопрос
+"Ваш любимый жанр под номером ${номер по порядку}". Каждый ответ записывается в массив данных
+genres
+
+P.S. Функции вызывать не обязательно*
+ */
+function showMyDB() {
+    if(personalMovieDB.privat===false){
+        return personalMovieDB
+    }
+};
+function writeYourGenres() {
+    const question=prompt(`Ваш любимый жанр под номером ${}`);
+    const number=
+}
