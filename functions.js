5ef53a093d75399f014678e83149ce410a1a4749
ZhykharievAnatolii <nekijt87@gmail.com>
2023-03-01 19:34:45 +0200

    add trim method for validation

--- functions.js
+++ functions.js
@@ -183,20 +183,7 @@
 //
 // }
 //
-// function rememberMyFilms() {
-//     for (let i = 0; i < 2; i++) {
-//         const a = prompt('Один из последних просмотренных фильмов?', ''),
-//             b = prompt('На сколько оцените его?', '');
-//
-//         if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-//             personalMovieDB.movies[a] = b;
-//             console.log('done');
-//         } else {
-//             console.log('error');
-//             i--;
-//         }
-//     }
-// }
+
 // // rememberMyFilms();
 // function detectPersonalLevel() {
 //     if (personalMovieDB.count < 10) {
@@ -239,3 +226,17 @@
 //     }
 // }
 // writeYourGenres();
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++) {
+        const a = prompt('Один из последних просмотренных фильмов?', '').trim();
+            const b = prompt('На сколько оцените его?', '');
+
+        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+            personalMovieDB.movies[a] = b;
+            console.log('done');
+        } else {
+            console.log('error');
+            i--;
+        }
+    }
+}
--- methodsOfStringsAndNumbers.js
+++ methodsOfStringsAndNumbers.js
@@ -1,26 +1,27 @@
-"use strict"
+// "use strict"
+//
+// const str='teSt';
+// const arr=[1,2,3,4,5,6,7,8]
+// // console.log(arr.length)
+//
+// // console.dir(Number)
+// // console.log(str[2])
+// // console.log(str.toUpperCase())
+// // console.log(str)
+// // const fruit='Some fruit';
+// // console.log(fruit.indexOf('fruit'));
+// // const logg='Hello world!';
+// // console.log(logg.slice(6));
+// // console.log(logg.substr(6,11));
+// // console.log(logg.substring(6));
+//
+// // Math- встроенная библиотека в обьект window
+//
+// const num= 12.2
+// console.log(Math.round(num))
+// const test='12.2px';
+// console.log(parseInt(test))
+// console.log(parseFloat(test))
+// // parseInt()- переводит число в другой метод исчисления
+// // parseFloat()- возвращает число с плавающей точкой
 
-const str='teSt';
-const arr=[1,2,3,4,5,6,7,8]
-// console.log(arr.length)
-
-// console.dir(Number)
-// console.log(str[2])
-// console.log(str.toUpperCase())
-// console.log(str)
-// const fruit='Some fruit';
-// console.log(fruit.indexOf('fruit'));
-// const logg='Hello world!';
-// console.log(logg.slice(6));
-// console.log(logg.substr(6,11));
-// console.log(logg.substring(6));
-
-// Math- встроенная библиотека в обьект window
-
-const num= 12.2
-console.log(Math.round(num))
-const test='12.2px';
-console.log(parseInt(test))
-console.log(parseFloat(test))
-// parseInt()- переводит число в другой метод исчисления
-// parseFloat()- возвращает число с плавающей точкой
