e060881ca8dbd6bac932aa84d880d15a8657c935
ZhykharievAnatolii <nekijt87@gmail.com>
2023-03-01 19:39:36 +0200

    add callback.js

---
+++ callbackFunctions.js
@@ -0,0 +1,8 @@
+'use strict'
+
+function first() {
+    //Do something
+    setTimeout(function () {
+        console.log(1)
+    },500)
+}
--- functions.js
+++ functions.js
@@ -226,17 +226,17 @@
 //     }
 // }
 // writeYourGenres();
-function rememberMyFilms() {
-    for (let i = 0; i < 2; i++) {
-        const a = prompt('Один из последних просмотренных фильмов?', '').trim();
-            const b = prompt('На сколько оцените его?', '');
-
-        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-            personalMovieDB.movies[a] = b;
-            console.log('done');
-        } else {
-            console.log('error');
-            i--;
-        }
-    }
-}
+// function rememberMyFilms() {
+//     for (let i = 0; i < 2; i++) {
+//         const a = prompt('Один из последних просмотренных фильмов?', '').trim();
+//             const b = prompt('На сколько оцените его?', '').trim().toLowerCase();
+//
+//         if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+//             personalMovieDB.movies[a] = b;
+//             console.log('done');
+//         } else {
+//             console.log('error');
+//             i--;
+//         }
+//     }
+// }
