e4f10060ccf709aaaabd364201487ef3642c507a
ZhykharievAnatolii <nekijt87@gmail.com>
2023-03-01 20:52:10 +0200

    upd about destruct

--- objects.js
+++ objects.js
@@ -11,17 +11,44 @@ const options={
     colors:{
         border:'black',
         background:'red'
+    },
+    makeTest: function () {
+        console.log('test')
     }
 }
+// console.log(options["colors"]["border"])
 // console.log(options.name);
 // console.log(options["widht"]);
 // delete options.name
 // console.log(options)
-for(let key in options){
-    if(typeof(options.key)==="object" ){
-        for (let i in options.key){
-            console.log(`Свойство ${key} имеет знаение ${options[key]}`)
-        }
-    }
-    console.log(`Свойство ${key} имеет знаение ${options[key]}`)
-}
+// let counter=0;
+// for(let key in options){
+//     if(typeof(options[key])==='object' ){
+//         for (let i in options[key]){
+//             console.log(`Свойство ${i} имеет знаение ${options[key][i]}`);
+//         }
+//     }else{console.log(`Свойство ${key} имеет знаение ${options[key]}`)};
+//     counter++
+//
+// }
+// console.log(counter)
+console.log(Object.keys(options).length);
+console.log(Object.values(options).length);
+options.makeTest();
+
+// Обьекты это структуры, которые в себе могут содержать любые типы данных в формате ключ-значение
+// Они могут быть вложены одни в другие
+// Для перебора есть for in
+// Object.keys();
+// Object.values();
+// Также в обьекте есть встроенные методы и свойства
+//
+// Можем самостоятельно создавть их
+//
+// Существуют аксессоры- это встроенные методы get и  set
+
+// Деструктуризация обьектов и массивов
+
+const {border, background}=options.colors
+
+console.log(border)
